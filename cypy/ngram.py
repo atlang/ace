491e4400b7a9b068f2ff235f11f16f23da86dd60
Cyrus Omar <cyology@gmail.com>
2012-12-02 17:34:39 -0500

    Updated ngram

--- cypy/ngram.py
+++ cypy/ngram.py
@@ -1,76 +1,114 @@
+def sum_leaves(d):
+    """Sums up the leaves of a nested dictionary."""
+    sum = 0
+    for value in d.itervalues():
+        if isinstance(value, dict):   # TODO: use correct checks
+            sum += sum_leaves(value)
+        elif isinstance(value, int):
+            sum += value
+    return sum
+
 class Corpus(object):
-	"""Represents the n-gram frequencies derived from some corpus."""
-	def __init__(self, data):
-		self.data = data
+    """Represents the n-gram frequencies derived from some corpus."""
+    def __init__(self, data):
+        self.data = data
 
-	data = None
-	"""A dictionary containing counts."""
+    data = None
+    """A dictionary containing counts."""
 
-	@classmethod
-	def from_file(cls, file):
-		# TODO: read file
-		return cls.from_lines(s)
+    @classmethod
+    def from_cmulm_filename(cls, filename):
+        return cls.from_cmulm_lines(open(filename).readlines())
 
-	@classmethod
-	def from_lines(cls, lines):
-		corpus = Corpus({})
+    @classmethod
+    def from_cmulm_lines(cls, lines, separator=" "):
+        """Creates a corpus from a sequence of lines produced by the CMULM library.
 
-		count = None
-		tokens = None
-		
-	    for line in lines:
-	    	if count is None:
-	    		tokens = tokenize(line)
-	    	else:
-	    		corpus.set_count(tokens, count)
-	    		count = None
-	    		
-	    return corpus
-	    
-	def set_count(self, tokens, count):
-		"""Sets the count of the given sequence of tokens."""
-		cur = self.data
-		tokens = list(tokens) # copy it so we can pop in loop
-		while len(tokens) > 1:
-			token = token.pop(0)
-			try:
-				cur = cur[token]
-			except KeyError:
-				cur = cur[token] = { }
+        That is, files of the format:
 
-		cur[tokens[0]] = count
+            tokens
+            count
+            tokens
+            count
+            ...
 
-	def get_count(self, tokens):
-		"""Returns the count of the given sequence of tokens.
+        Where tokens is a separator-separated sequence of tokens and count is the 
+        count of the sequence of tokens on the previous line.
+        """
+        corpus = Corpus({})
 
-		Sums over the leaves if tokens is a partial sequence.
-		"""
-		data = self.data
-		tokens = list(tokens) # copy it so we can pop in loop
-		while len(tokens) > 1:
-			token = token.pop(0)
-			try:
-				cur = cur[token]
-			except KeyError:
-				return 0
+        tokens = None
+        
+        for line in lines:
+            if tokens is None:
+                tokens = line.strip().split(separator)
+            else:
+                count = int(line)
+                corpus.set_count(tokens, count)
+                assert corpus.get_count(tokens) == count
+                tokens = None
+                
+        return corpus
+        
+    def set_count(self, tokens, count):
+        """Sets the count of the given sequence of tokens."""
+        cur = self.data
+        tokens = list(tokens) # copy it so we can pop in loop
+        while len(tokens) > 1:
+            token = tokens.pop(0)
+            try:
+                cur = cur[token]
+            except KeyError:
+                cur[token] = { }
+                cur = cur[token]
+        cur[tokens[0]] = count
 
-		c = cur[tokens[0]]
-		if isinstance(c, dict):  # TODO: use correct way of checking for dictionary
-			return sum_leaves(c) # TODO: implement sum_leaves
-		else:
-			return c 
-		return cur[tokens[0]]	
+    def get_count(self, tokens):
+        """Returns the count of the given sequence of tokens.
 
-	def prob(self, token, lead_tokens):
-		"""Returns the probability of the given tokens given the lead tokens.
+        Sums over the leaves if tokens is a partial sequence.
+        """
+        cur = self.data
+        if len(tokens) == 0:
+            return sum_leaves(cur)
 
-			>>> corpus.prob('in', ['the', 'cat'])
-			0.9
-		"""
-		lead_count = float(self.get_count(lead_tokens))
-		tokens = list(lead_tokens); tokens.append(token)
-		count = float(self.get_count(tokens))
-		return count / lead_count
+        tokens = list(tokens) # copy it so we can pop in loop
+        while len(tokens) > 1:
+            token = tokens.pop(0)
+            try:
+                cur = cur[token]
+            except KeyError:
+                return 0
 
-# TODO: write tests
+        c = cur[tokens[0]]
+        if isinstance(c, dict):  # TODO: use correct way of checking for dictionary
+            return sum_leaves(c)
+        else:
+            return c
 
+    def prob(self, token, lead_tokens):
+        """Returns the probability of the given tokens given the lead tokens.
+
+            >>> corpus.prob('in', ['the', 'cat'])
+            0.9
+
+        """
+        lead_count = float(self.get_count(lead_tokens))
+        tokens = list(lead_tokens); tokens.append(token)
+        count = float(self.get_count(tokens))
+        return count / lead_count
+
+def test_filename(filename):
+    lines = open(filename).readlines()
+    corpus = Corpus.from_cmulm_lines(lines)
+    print corpus.get_count(["!=", "null"])
+    print corpus.prob("&&", ["!=", "null"])
+
+if __name__ == "__main__":
+    import sys
+    try:
+        filename = sys.argv[1]
+    except:
+        print "Need a filename to test with."
+    else:
+        test_filename(filename)
